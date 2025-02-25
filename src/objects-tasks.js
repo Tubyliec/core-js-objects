60c9bd7b808019f6342ca377ad3fd46ee1d9360b
tubyliec <antikruk@vivaldi.net>
2025-02-25 23:22:52 +0300

    feat: add third task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -64,8 +64,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const targetObject = { ...obj };
+  Object.keys(targetObject).forEach((key) => {
+    if (keys.includes(key)) delete targetObject[key];
+  });
+  return targetObject;
 }
 
 /**
